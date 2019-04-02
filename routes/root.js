bec5fb9d9113b4f270863953ade55ae0ae684ebe
jogoo8254 <kev_inc88@hotmail.com>
2019-04-01 17:09:40 -0700

    backup old changes to root

--- routes/root.js
+++ routes/root.js
@@ -4,7 +4,13 @@ const router = express.Router();
 router.get('/',(req,res)=>{
     res.render('home',{username: res.locals.username});
 });
+router.get('/clucks',(req,res)=>{
+    res.render('home');
+});
 
+router.get('/clucks/new',checkIfAuthenticated,(req,res)=>{
+    res.render('new_cluck');
+});
 router.get('/login',(req,res)=>{
     res.render('login');
 });
@@ -16,6 +22,23 @@ router.post('/login',(req,res)=>{
     const { username } = req.body;
     res.cookie('username', username, {expires: ONE_DAY})
     res.redirect('/');
+})
+
+router.post('/clucks',(req,res)=>{
+    const content = req.body.content;
+    const image_url = req.body.image_url;
+    knex.insert({
+        username: req.cookies.username,
+        image_url: image_url,
+        content: content
+    })
+    .into('cluck')
+    .then((record)=>{
+        console.log(`record: ${record} created!`);
+        res.redirect('/');
+    }).catch((err) => {
+        res.send('<div>sorry something went wrong</div>')
+    })
 })
 
 router.post('/sign_in', (req,res)=>{
